1cbc7a8111c99576c7c190d44d68ebc69c659159
Jenny Yu <49422436+jxy608@users.noreply.github.com>
2025-02-28 23:27:25 -0500

    add to-do templates

--- src/app.js
+++ src/app.js
@@ -2,11 +2,70 @@ import Phaser from "phaser";
 
 class GameScene extends Phaser.Scene {
   preload() {
-    // Your code here!
+    this.load.image("player", "assets/player.png");
+
+    this.load.image("wallHorizontal", "assets/wallHorizontal.png");
+    this.load.image("wallVertical", "assets/wallVertical.png");
+
+    this.load.image("coin", "assets/coin.png");
   }
 
+  /**
+   * Called once. Create any objects you need here!
+   */
   create() {
-    // Your code here!
+    // TODO 1.1: create the player
+
+    // TODO 1.2: add gravity to make the player fall
+
+    // TODO 1.3: add arrow keys
+
+    // TODO 4.1: create the walls
+
+    // TODO 4.2: make the player collide with walls
+
+    // TODO 6.1: create the coin
+
+    // TODO 6.2: initialize a score counter
+  }
+
+  /**
+   * Creates the walls of the game
+   */
+  createWalls() {
+    // TODO 5: add walls
+  }
+
+  /**
+   * Phaser calls this function once a frame (60 times a second).
+   *
+   * Use this function to move the player in response to actions,
+   * check for win conditions, etc.
+   */
+  update() {
+    // TODO 2: implement update game state 
+  }
+
+  /**
+   * Handles moving the player with the arrow keys
+   */
+  movePlayer() {
+    // TODO 3.1: move the player left or right
+    // TODO 3.2: jump the player
+  }
+
+  /**
+   * Check to see whether the player has collided with any coins
+   */
+  checkCoinCollisions() {
+    // TODO 7: check for coin collision and update score accordingly
+  }
+
+  /**
+   * Move the coin to a different random location
+   */
+  moveCoin() {
+    // TODO 8: move coin when picked up
   }
 }
 
